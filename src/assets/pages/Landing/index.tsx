5f61d08f48085ce284dbdf2e5a53e2f32a116c16
Leonidas Amorim <leonidasamorim@gmail.com>
2020-08-09 09:31:12 -0300

    first commit

---
+++ src/assets/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { View, Image, Text } from "react-native";
+
+import styles from "./styles"; 
+
+
+const Landing() {
+  
+  return (
+    <View />
+  );
+};
+
+export default Landing;
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { View, Image, Text } from "react-native";
+
+import styles from "./styles"; 
+
+
+function Landing() {
+  return (
+    <View>
+      <Text>Olá</Text>
+      </View>
+  );
+};
+
+export default Landing;
