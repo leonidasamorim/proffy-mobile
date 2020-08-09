e3289eaee4b490ebdd414c3be22d897934a3fe90
Leonidas Amorim <leonidasamorim@gmail.com>
2020-08-09 10:15:42 -0300

    Landing Page with style

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,14 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 
-import styles from "./styles"; 
+import styles from "./styles";
+
+import landingImg from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClassesIcon from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
+
+const Landing: React.FC = () => {
 
 
-function Landing() {
   return (
-    <View>
-      <Text>Olá</Text>
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {"\n"}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton
+          onPress={null}
+          style={[styles.button, styles.buttonPrimary]}
+        >
+          <Image source={studyIcon} />
+
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={null}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon} />
+
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
       </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 100 conexões já realizadas{" "}
+        <Image source={heartIcon} />
+      </Text>
+    </View>
   );
 };
 
